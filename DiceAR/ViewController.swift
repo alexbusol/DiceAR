1a72ff747de3e2569927616af3a3556520aa53fe
alexbusol <ovbusol@crimson.ua.edu>
2018-07-13 17:40:43 +0300

    Added horizontal plane detection using ARKit. Added a transparent grid visualization to the detected plane.

--- DiceAR/ViewController.swift
+++ DiceAR/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints] //shows the surface points as the phone is looking for a horizontal surface
         // Set the view's delegate
         sceneView.delegate = self
         
@@ -42,6 +42,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .horizontal //enabling horizontal plane detection
+        
 
         // Run the view's session
         sceneView.session.run(configuration)
@@ -52,5 +54,27 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Pause the view's session
         sceneView.session.pause()
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) { //when a new horizontal plane is detected. ARAnchor - real world position in the horizontal plane.
+        if anchor is ARPlaneAnchor {
+            print("Plane detected")
+            let planeAnchor = anchor as! ARPlaneAnchor //change the anchor type
+            let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z)) //dont use y here
+            
+            let planeNode = SCNNode()
+            planeNode.position = SCNVector3(x: planeAnchor.center.x, y: 0, z: planeAnchor.center.z) //y is 0 becaue we dont want the node to be above the detected plane
+            planeNode.transform = SCNMatrix4MakeRotation(-Float.pi/2, 1, 0, 0) //will need to rotate the plane because it's horizontal by default. float.pi/2 = 90 degrees rotation counterclockwise. add minus to make it clockwise. 1 in the x means that we are rotating it around the x axis.
+            
+            let gridMaterial = SCNMaterial()
+            gridMaterial.diffuse.contents = UIImage(named: "art.scnassets/grid.png")
+            
+            plane.materials = [gridMaterial]
+            planeNode.geometry = plane
+            
+            node.addChildNode(planeNode)
+        } else {
+            return
+        }
     }
 }
