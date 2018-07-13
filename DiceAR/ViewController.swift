012a033ca3c58593dd7edfdaa02933611bd71536
alexbusol <ovbusol@crimson.ua.edu>
2018-07-13 18:38:19 +0300

    Added the ability to place the dice onto a plane wherever the user taps on the screen. Used random number generator to show random dice faces with spinning animation.

--- DiceAR/ViewController.swift
+++ DiceAR/ViewController.swift
@@ -56,6 +56,49 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
     
+    
+    //recognize touches on the screen
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            let touchLocation = touch.location(in: sceneView) //2d coordinate due to the screen being 2d.
+            
+            let results = sceneView.hitTest(touchLocation, types: .existingPlaneUsingExtent) //searches for real world images corresponding to the place pressed on the screen
+            
+            if let hitResult = results.first {
+                print(hitResult) //printing the coordinate data from the touch, as well as the real world position detected after the touch
+                
+                let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")
+                
+                if let diceNode = diceScene?.rootNode.childNode(withName: "Dice", recursively: true) {
+                
+                diceNode.position = SCNVector3(hitResult.worldTransform.columns.3.x,
+                                               hitResult.worldTransform.columns.3.y + diceNode.boundingSphere.radius,
+                                               hitResult.worldTransform.columns.3.z) //need to change the y position a bit because it places the center of the object at the plane, making half of it below the plane.
+                    
+                sceneView.scene.rootNode.addChildNode(diceNode)
+                    
+                    let randomXNum = Float(arc4random_uniform(4) + 1) * (Float.pi/2) //creating rotation angles for x and z. rotation in Y axis is not necessary.
+                    let randomZNum = Float(arc4random_uniform(4) + 1) * (Float.pi/2)
+                    
+                    diceNode.runAction(SCNAction.rotateBy(x: CGFloat(randomXNum * 4), //increasing the number of rotations.
+                                                          y: 0,
+                                                          z: CGFloat(randomZNum * 4),
+                                                          duration: 0.5))
+                }
+            }
+            /*
+            if !results.isEmpty {
+                print("the plane was touched")
+            } else {
+                print("The touch did not connect ot the plane")
+            }*/
+            
+            
+        }
+    }
+    
+    
+    //Responsible for placing a transparent grid visualization onto a horizontal plane when it gets detected. it is important to rotate the plane 90 degrees counterclockwise because it is vertical by default. a call to SCNMAtrix4makeRotation accomplishes just that.
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) { //when a new horizontal plane is detected. ARAnchor - real world position in the horizontal plane.
         if anchor is ARPlaneAnchor {
             print("Plane detected")
